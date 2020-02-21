9f50c5a324bc7dc660091ca28730094b01e05401
Nathan Knowler <nathan@knowler.me>
2020-02-21 01:40:52 -0700

    Add basic instructions to config

--- packages/example/sage.config.js
+++ packages/example/sage.config.js
@@ -1,8 +1,11 @@
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 module.exports = {
+  /** Specify your assets here. */
   assets: ['scripts/main.js', 'styles/main.css'],
-  webpack: config => {
+
+  /** Override the webpack config here. Note: it’s just the full webpack config object. */
+  webpack: (config, {webpack}) => {
     config.plugins.push(new WebpackAssetsManifest());
 
     return config;
